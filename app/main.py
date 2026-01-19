5030210aacd902e46854a2d8a327db58e25acdc7
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2026-01-19 17:47:20 -0500

    Comment out previous code to be more SQL friendly

--- app/main.py
+++ app/main.py
@@ -14,32 +14,32 @@ class Item(BaseModel):
 
 items = []
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
-@app.get("/items")
-def read_all_items():
-    return items
+# @app.get("/items")
+# def read_all_items():
+#     return items
 
-@app.get("/items/{item_id}")
-def read_item_id(item_id : int, in_stock: bool = True) -> Item:
-    if item_id < len(items) and in_stock:
-        return items[item_id]
-    else:
-        raise HTTPException(status_code=404, detail=f"Item of id {item_id} Not Found or query parameter is invalid")
+# @app.get("/items/{item_id}")
+# def read_item_id(item_id : int, in_stock: bool = True) -> Item:
+#     if item_id < len(items) and in_stock:
+#         return items[item_id]
+#     else:
+#         raise HTTPException(status_code=404, detail=f"Item of id {item_id} Not Found or query parameter is invalid")
 
 
-@app.post("/items")
-def create_item(item_id: int, item_name: str, item_description: str,
-    item_price: float, item_in_stock: bool, item: Item):
-    item.id = item_id
-    item.name = item_name
-    item.description = item_description
-    item.price = item_price
-    item.in_stock = item_in_stock
-    items.append(item)
-    return items
+# @app.post("/items")
+# def create_item(item_id: int, item_name: str, item_description: str,
+#     item_price: float, item_in_stock: bool, item: Item):
+#     item.id = item_id
+#     item.name = item_name
+#     item.description = item_description
+#     item.price = item_price
+#     item.in_stock = item_in_stock
+#     items.append(item)
+#     return items
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item_name: str, item_description: str,
