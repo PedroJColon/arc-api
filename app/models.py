b833e03aa38b22d9603fe3574621e4ff4a25fd2c
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2026-01-22 20:22:38 -0500

    Add models.py file for better organization

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from sqlmodel import Field, SQLModel
+
+class ItemBase(SQLModel):
+    name: str = Field(index=True)
+    description: str
+    price: float = Field(default=None, index=True)
+    in_stock: bool | None = Field(default=None, index=True)
+
+class Item(ItemBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+class ItemCreate(ItemBase):
+    pass
+
+class ItemPublic(ItemBase):
+    id: int
+
+class ItemUpdate(SQLModel):
+    name: str | None = None
+    description: str | None = None
+    price: float | None = None
