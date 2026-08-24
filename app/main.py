4624d530b21fb1774de8af448f3b97d9cddaeb68
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2026-08-24 16:39:06 -0400

    Made change to use lifespan event from FastAPI

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, HTTPException
 from sqlmodel import Session, SQLModel, create_engine, select
 import models as api_models
@@ -19,10 +21,9 @@ def create_db_and_tables():
 # With setup done, call FastAPI to run app
 app = FastAPI()
 
-# NOTE: While it is supposed to be lifetime events, I found it difficult to try and get it to work properly. 
-# For sake of simplicity, I have used to old event callback
-@app.on_event("startup")
-def on_startup():
+# Async function for handling lifespan of application
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_db_and_tables()
 
 # Create item and have it be saved to sql database thanks to session and engine
@@ -42,7 +43,7 @@ def read_items():
         items = session.exec(select(api_models.Item)).all()
         return items
 
-# Read item of selected ID, follows similar steps as previous function 
+# Read item of selected ID, follows similar steps as previous function
 @app.get("/items/{item_id}", response_model=api_models.ItemPublic)
 def read_selected_item(item_id: int):
     with Session(engine) as session:
@@ -57,14 +58,14 @@ def update_item(item_id: int, item: api_models.ItemUpdate):
     with Session(engine) as session:
         current_item = session.get(api_models.Item, item_id)
         if not current_item:
-            raise HTTPException(status_code=404, detail="Hero Not Found")
+            raise HTTPException(status_code=404, detail="Item of {item_id} Not Found")
         item_data = item.model_dump(exclude_unset=True)
         current_item.sqlmodel_update(item_data)
         session.add(current_item)
         session.commit()
         session.refresh(current_item)
         return current_item
-        
+
 
 # Delete item from database file, simple as read and read selected
 @app.delete("/items/{item_id}")
